83e3daae57821e558b8c1b3b2e3bd4ef34ba23d4
Fraser Morrison <frazemor@hotmail.com>
2023-07-30 22:56:11 -0400

    Added tags to db

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -23,11 +23,30 @@ pub fn init_db(handle: &AppHandle) -> Result<Connection> {
     let conn = Connection::open(dir.join("db.sqlite"))?;
 
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS pictures (
-            id INTEGER PRIMARY KEY,
-            path TEXT NOT NULL,
-            params TEXT NOT NULL,
-            tags TEXT NOT NULL,
+        "CREATE TABLE IF NOT EXISTS images (
+            id INTEGER NOT NULL PRIMARY KEY,
+            path TEXT NOT NULL UNIQUE,
+            params TEXT,
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS tags (
+            id INTEGER NOT NULL PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS image_tags (
+            id INTEGER NOT NULL PRIMARY KEY,
+            image_id INTEGER NOT NULL,
+            tag_id INTEGER NOT NULL,
+            UNIQUE (image_id, tag_id),
+            FOREIGN KEY (image_id) REFERENCES images(id),
+            FOREIGN KEY (tag_id) REFERENCES tags(id),
         )",
         [],
     )?;
@@ -35,14 +54,47 @@ pub fn init_db(handle: &AppHandle) -> Result<Connection> {
     Ok(conn)
 }
 
-// Add update metadata function
-
-// Add
-
-pub fn add_song(conn: &Connection) -> Result<()> {
+pub fn add_image(conn: &Connection, path: &str) -> Result<()> {
     conn.execute(
-        "INSERT INTO songs (title, url, thumbnail, path, downloaded) VALUES (?1, ?2, ?3, ?4, ?5)",
+        "INSERT INTO images (path) values (?1) ON CONFLICT(path) DO NOTHING",
         [],
+    )?;
+    Ok(())
+}
+
+pub fn move_image(conn: &Connection, old_path: &str, new_path: &str) -> Result<()> {
+    conn.execute(
+        "UPDATE images SET path=?1 WHERE path=?2",
+        [old_path, new_path],
+    )?;
+    Ok(())
+}
+
+pub fn remove_image(conn: &Connection, path: &str) -> Result<()> {
+    conn.execute("DELETE FROM images WHERE path=?1", [path])?;
+    Ok(())
+}
+
+pub fn add_params(conn: &Connection, path: &str, params: &str) -> Result<()> {
+    conn.execute(
+        "INSERT INTO images (path, params) values (?1, ?2) ON CONFLICT(path) DO UPDATE SET params=?2",
+        [path, params],
+    )?;
+    Ok(())
+}
+
+pub fn create_tag(conn: &Connection, name: &str) -> Result<()> {
+    conn.execute(
+        "INSERT INTO tags (name) values (?1) ON CONFLICT(name) DO NOTHING",
+        [name],
+    )?;
+    Ok(())
+}
+
+pub fn add_tag_to_image(conn: &Connection, image_id: i32, tag_id: i32) -> Result<()> {
+    conn.execute(
+        "INSERT INTO image_tags (image_id, tag_id) values (?1, ?2) ON CONFLICT(image_id, tag_id) DO NOTHING",
+        [image_id, tag_id],
     )?;
     Ok(())
 }
