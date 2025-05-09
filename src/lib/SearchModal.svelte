c77322e6949254e05e1034c5f88c46b1c299b689
Fraser Morrison <frazemor@hotmail.com>
2025-05-08 23:34:46 -0400

    removed unused css

--- src/lib/SearchModal.svelte
+++ src/lib/SearchModal.svelte
@@ -48,39 +48,4 @@
     gap: 0.5rem;
     margin: 0.5rem 15%;
   }
-
-  .tag-text {
-    background-color: rgba(0, 0, 0, 0.5);
-    padding: 1em;
-    border-radius: 1em;
-    height: 3rem;
-    padding: 0.25rem 0.75rem;
-    width: 100%;
-    flex-wrap: nowrap;
-    overflow: visible;
-  }
-
-  .tag {
-    display: inline-block;
-    border-radius: 25px;
-    border: 1px solid black;
-    padding: 0.25em 0.5em;
-    margin-right: 0.5em;
-    margin-bottom: 0.5em;
-    text-align: center;
-    background-color: rgba(0, 0, 0, 0);
-    color: #fff;
-  }
-
-  .remove-tag:hover {
-    background-color: rgba(255, 0, 0, 0.5);
-  }
-
-  .add-tag {
-    min-width: 1em;
-    font-size: 1.25rem;
-    font-weight: 500;
-    font-family: inherit;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-  }
 </style>
